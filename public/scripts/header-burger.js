b122a8d6dad6ac156d00eac0c16033d08a3c6798
Thoedor Tsvetkoff <oxidemanganese@zohomail.com>
2025-11-05 10:12:05 +0300

    feat: написан ModernizationLayout, изменены заголовки

--- public/scripts/header-burger.js
+++ public/scripts/header-burger.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.header__burger');
   const nav = document.querySelector('.header__nav');
 
-  if (!burger || !nav) return;
-
   burger.addEventListener('click', () => {
     burger.classList.toggle('header__burger--active');
     nav.classList.toggle('header__nav--open');
